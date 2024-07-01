548a3bc75f632c2770ff637b75d1137c03d6d1a2
prave1n <pravein2002@gmail.com>
2024-07-01 08:23:13 +0800

    bug: setupTests failed to deploy

--- frontend/src/setupTests.js
+++ frontend/src/setupTests.js
@@ -1,15 +1,2 @@
-import { setupTestStore } from "./store/store";
-import { Provider } from "react-redux";
-import React from "react";
-import { render } from "@testing-library/react";
 import '@testing-library/jest-dom';
 
-export function renderWithRedux(
-    ui,
-    { initialState, store = setupTestStore().store } = {}
-  ) {
-    return {
-      ...render(<Provider store={store}>{ui}</Provider>),
-      store,
-    };
-  }
